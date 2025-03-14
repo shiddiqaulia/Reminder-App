d3aed71ed0593863b76f068d8f256048140a8542
shiddiqaulia <diqi1805@gmail.com>
2025-03-14 15:39:52 +0700

    Final server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-
+  
 // Koneksi ke Database PostgreSQL
 const db = pgp({
   user: process.env.PG_USER,
